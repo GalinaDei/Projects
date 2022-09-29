2048e0775a6c20cac9e2d89baa1f1987e6ba408c
Galina <gal-dej@yandex.ru>
2022-09-29 15:53:04 +0300

    Two new projects

---
+++ Example01_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your username");
+string username=Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
+Console.WriteLine("!");
