8a994658c05f6b9a356023863050123352f9a428
Galina <gal-dej@yandex.ru>
2022-09-29 18:47:30 +0300

    Greeteng

---
+++ Example04_Greeting/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your username");
+string username = Console.ReadLine();
+if(username == "Galina")
+{
+    Console.WriteLine("Nice to meet you, Galina!");
+}
+else
+{
+    Console.Write ("Hello, ");
+    Console.Write (username);
+}
