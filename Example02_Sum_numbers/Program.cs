7833904f1e4027cf72ba33f682b8c6155f38c627
Galina <gal-dej@yandex.ru>
2022-09-29 18:10:49 +0300

    Random number

--- Example02_Sum_numbers/Program.cs
+++ Example02_Sum_numbers/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+Console.Write("numberA - ");
+Console.WriteLine(numberA);
+Console.Write("numberB - ");
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
